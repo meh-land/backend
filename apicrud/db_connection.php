d920ea160867b527b65beeb84993c9bb3db31467
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-11-28 11:03:21 +0200

    env variables used

--- apicrud/db_connection.php
+++ apicrud/db_connection.php
@@ -1,11 +1,24 @@
 <?php
+
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+
+$dotenv->load();
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$db_conn = mysqli_connect("localhost", "root", "Id:19016729", "devooti",3307);
+$DB_USERNAME = $_ENV["DB_USERNAME"];
+$DB_PWD = $_ENV["DB_PWD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+
+$db_conn = mysqli_connect("127.0.0.1", $DB_USERNAME, $DB_PWD, $DB_NAME ,$DB_PORT);
 
 // Check connection
 if($db_conn === false){
